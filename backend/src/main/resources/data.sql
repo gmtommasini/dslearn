d9b95f58deaa5b5f4b9fa4bb48c8a052ce277576
gmtommasini <gmtommasini@gmail.com>
2021-08-15 11:29:59 -0400

    course offer

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -13,5 +13,9 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
 
-INSERT INTO tb_courses (name, img_Uri, img_Gray_Uri ) VALUES ('"Bootcamp HTML', '"https://c.pxhere.com/images/07/b2/f8ed07d6f1a9c740e7703cbb99ee-1443423.jpg!d','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVh2uM-DebaaY88YfkDGLpfhiKi054rCO2GQ&usqp=CAU');
+INSERT INTO tb_courses (name, img_Uri, img_Gray_Uri ) VALUES ('Bootcamp HTML', 'https://c.pxhere.com/images/07/b2/f8ed07d6f1a9c740e7703cbb99ee-1443423.jpg!d','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVh2uM-DebaaY88YfkDGLpfhiKi054rCO2GQ&usqp=CAU');
 INSERT INTO tb_courses (name, img_Uri, img_Gray_Uri ) VALUES ('SpringBoot', 'https://c.pxhere.com/images/07/b2/f8ed07d6f1a9c740e7703cbb99ee-1443423.jpg!d','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVh2uM-DebaaY88YfkDGLpfhiKi054rCO2GQ&usqp=CAU');
+
+
+INSERT INTO tb_offers (edition, start_Moment, end_Moment, course_id ) VALUES ('1.0',TIMESTAMP WITH TIME ZONE '2021-07-11T04:00:00Z',TIMESTAMP WITH TIME ZONE '2021-08-06T23:59:59Z','1');
+INSERT INTO tb_offers (edition, start_Moment, end_Moment, course_id ) VALUES ('2.0',TIMESTAMP WITH TIME ZONE '2021-09-11T04:00:00Z',TIMESTAMP WITH TIME ZONE '2021-10-06T23:59:59Z','1');
